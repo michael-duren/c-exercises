a5977777c401491eb33d0ae6f720d924399c3499
Michael Duren <michaeld@michaelduren.com>
2024-04-23 21:01:31 -0500

    update

--- 01/main.c
+++ 01/main.c
@@ -1,19 +1,19 @@
 #include "myfunctions.h"
+#include <limits.h>
+#include <stdio.h>
 // #include <stdio.h>
 
 #define TAB_LENGTH 4
 
+// determine the length of char, short, int, and long signed and unsigned
+
 int main() {
-  remove_comments();
-  // int c;
-  //
-  // while ((c = getchar()) != EOF) {
-  //   if (c == '\t') {
-  //     for (int i = 0; i < TAB_LENGTH; i++) {
-  //       printf(" ");
-  //     }
-  //   } else {
-  //     printf("%d \n", c);
-  //   }
-  // }
+  printf("The max value of char is: %d\n", CHAR_MAX);
+  printf("The min value of char is: %d\n", CHAR_MIN);
+  printf("The max value of int is: %d\n", INT_MAX);
+  printf("The min value of int is: %d\n", INT_MIN);
+  printf("The max value of short is: %d\n", SHRT_MAX);
+  printf("The min value of short is: %d\n", SHRT_MIN);
+  printf("The max value of long is: %ld\n", LONG_MAX);
+  printf("The min value of long is: %ld\n", LONG_MIN);
 }
--- 02/main.c
+++ 02/main.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
 #include <limits.h>
+#include <stdio.h>
 
 int main() {
-    unsigned long example = -1;
-    printf("The min and max sizes of int on mac os: %d %d\n", INT_MIN, INT_MAX);
-    printf("Size of int: %zu bytes\n", sizeof(int));
-    printf("%zu", example);
+  unsigned long example = -1;
+  printf("The min and max sizes of int on mac os: %d %d\n", INT_MIN, INT_MAX);
+  printf("Size of int: %zu bytes\n", sizeof(int));
+  printf("%zu", example);
 }
